6a55b02cf6717ede880f088282cd0471be563e9c
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-07-23 20:25:59 -0300

    Lógica do Programa

---
+++ script.js
@@ -0,0 +1,17 @@
+var product = document.getElementById("product");
+var price = document.getElementById("price");
+var calcBtn = document.getElementById("calcBtn");
+var promo = document.getElementById("promo");
+var product3Message = document.getElementById("product3");
+
+calcBtn.addEventListener("click", calcular);
+function calcular() {
+  var product3 = parseInt(price.value) * 0.5;
+  var promoPrice = parseInt(price.value) * 3 - product3;
+  promo.textContent =
+    product.value.toUpperCase() +
+    " - Promoção: Leve 3 por R$: " +
+    promoPrice.toFixed(2);
+  product3Message.textContent =
+    "O 3º produto custa apenas R$: " + product3.toFixed(2);
+}
